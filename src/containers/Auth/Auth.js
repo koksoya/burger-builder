3bae6dc0c4cf6ae8c0be91eb794cd4cfc3875bc2
koksoya <b.koksoya@gmail.com>
2020-05-02 01:01:00 -0400

    Fixed Redirect to Front Page, Used updateObject() in the entire app, shared the checkValidity() method

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -7,7 +7,7 @@ import Button from '../../components/Burger/UI/Button/Button'
 import Spinner from '../../components/Burger/UI/Spinner/Spinner'
 import classes from './Auth.css'
 import * as actions from '../../store/actions/index'
-
+import { updateObject, checkValidity } from '../../shared/utility'
 
 class Auth extends Component {
     state = {
@@ -44,40 +44,21 @@ class Auth extends Component {
         isSignup: true
     }
 
-    componentDidMount () {
+    componentDidMount() {
         if (!this.props.building && this.props.authRedirectPath !== '/') {
             this.props.onSetAuthRedirectPath()
         }
     }
 
-    checkValidity(value, rules) {
-        let isValid = true
-
-        if (rules.required) {
-            isValid = value.trim() !== '' && isValid
-        }
-
-        if (rules.minLength) {
-            isValid = value.length >= rules.minLength && isValid
-        }
-
-        if (rules.maxLength) {
-            isValid = value.length <= rules.maxLength && isValid
-        }
-
-        return isValid
-    }
-
+ 
     inputChangedHandler = (event, controlName) => {
-        const updatedControls = {
-            ...this.state.controls,
-            [controlName]: {
-                ...this.state.controls[controlName],
+        const updatedControls = updateObject(this.state.controls, {
+            [controlName]: updateObject(this.state.controls[controlName], {
                 value: event.target.value,
-                valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
+                valid: checkValidity(event.target.value, this.state.controls[controlName].validation),
                 touched: true
-            }
-        }
+            })
+        })
         this.setState({ controls: updatedControls })
     }
 
@@ -126,7 +107,7 @@ class Auth extends Component {
                 <p>{this.props.error.message}</p>
             )
         }
-        
+
         let authRedirect = null
         if (this.props.isAuthenticated) {
             authRedirect = <Redirect to={this.props.authRedirectPath} />
